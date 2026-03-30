7feb968695342a2c58520699efd7d14f0bbc3b07
alex <arojop27@gmail.com>
2026-03-30 16:04:37 +0200

    Front - feat: creación de weComponent card y header + /home a editar

---
+++ frontend/assets/components/card.js
@@ -0,0 +1,36 @@
+class CardComponent extends HTMLElement {
+    connectedCallback() {
+        const icono = this.getAttribute('icono');
+        const titulo = this.getAttribute('titulo');
+        const descripcion = this.getAttribute('descripcion');
+        const botonText = this.getAttribute('boton-text');
+
+        this.innerHTML = `
+        <div class="card text-center h-100 border-0 shadow-sm position-relative card-component">
+            <!-- Icono -->
+            <div class="d-flex align-items-center justify-content-center mx-auto mt-3 mb-4 icono-card">
+                <span class="material-symbols-outlined">
+                    ${icono}
+                </span>
+            </div>
+
+            <!-- Título -->
+            <h2 class="fuente-titular fw-bold mb-2">
+                ${titulo}
+            </h2>
+
+            <!-- Descripción -->
+            <p class="text-muted mb-4">
+                ${descripcion}
+            </p>
+
+            <!-- Botón -->
+            <button class="btn w-100 fw-bold">
+                ${botonText}
+            </button>
+        </div>
+        `;
+    }
+}
+
+customElements.define('app-card', CardComponent);
--- frontend/assets/components/header.js
+++ frontend/assets/components/header.js
@@ -1,21 +1,29 @@
-class Header extends HTMLElement{
-    connectedCallback(){
+class Header extends HTMLElement {
+    connectedCallback() {
 
         this.innerHTML = `
         <header>
-            <nav>
-                <a href="../views/dashboard.html"><h1 class="fuente-titular">VOTAPP</h1></a>
-                <div class="">
-                    <a>Elecciones</a>
-                    <a>Resultados</a>
-                    <a>Candidatos</a>
-                </div>
-                <div>
-                    <a></a>
-                </div>
-            </nav>
-        </header>
+        <nav class="d-flex justify-content-between align-items-center p-4">
+            <a class=" text-decoration-none" href="./home.html">
+                <h1 class="fuente-titular">VOTAPP</h1>
+            </a>
+
+            <div class="d-flex gap-5">
+                <a>Elecciones</a>
+                <a>Resultados</a>
+                <a>Candidatos</a>
+            </div>
+
+            <div class="d-flex gap-3 align-items-center">
+                <a><span class="material-symbols-outlined">info</span></a>
+                <a><span class="material-symbols-outlined">logout</span></a>
+                <a class="d-block" style="width: 40px; height: 40px;">
+                    <img class="w-100 h-100 object-fit-cover rounded-circle" src="../assets/static/user.png">
+                </a>
+            </div>
+        </nav>
+    </header>
         `
     }
 }
-coustomElements.define('app-header', Header);
+customElements.define('app-header', Header);

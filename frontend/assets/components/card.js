1106b30d3a930bd315d923afa43e43f7672f2cc9
alex <arojop27@gmail.com>
2026-04-15 15:30:29 +0200

    Update: UI home + header

--- frontend/assets/components/card.js
+++ frontend/assets/components/card.js
@@ -5,29 +5,27 @@ class CardComponent extends HTMLElement {
         const descripcion = this.getAttribute('descripcion');
         const botonText = this.getAttribute('boton-text');
 
-        this.innerHTML = `
+        this.innerHTML = /*html*/ `
         <div class="card text-center h-100 border-0 shadow-sm position-relative card-component">
-            <!-- Icono -->
             <div class="d-flex align-items-center justify-content-center mx-auto mt-3 mb-4 icono-card bg-light">
+        
                 <span class="material-symbols-outlined">
                     ${icono}
                 </span>
             </div>
-
-            <!-- Título -->
+        
             <h2 class="fuente-titular fw-bold mb-2">
                 ${titulo}
             </h2>
-
-            <!-- Descripción -->
+        
             <p class="text-muted mb-4">
                 ${descripcion}
             </p>
-
-            <!-- Botón -->
+        
             <button class="btn w-100 fw-bold">
                 ${botonText}
             </button>
+        
         </div>
         `;
     }
--- frontend/assets/components/header.js
+++ frontend/assets/components/header.js
@@ -1,28 +1,29 @@
 class Header extends HTMLElement {
     connectedCallback() {
 
-        this.innerHTML = `
+        this.innerHTML = /*html*/ `
         <header>
-        <nav class="d-flex justify-content-between align-items-center p-4">
-            <a class=" text-decoration-none" href="./home.html">
-                <h1 class="fuente-titular">VOTAPP</h1>
-            </a>
-
-            <div class="d-flex gap-5">
-                <a>Elecciones</a>
-                <a>Resultados</a>
-                <a>Candidatos</a>
-            </div>
-
-            <div class="d-flex gap-3 align-items-center">
-                <a><span class="material-symbols-outlined">info</span></a>
-                <a><span class="material-symbols-outlined">logout</span></a>
-                <a class="d-block" style="width: 40px; height: 40px;">
-                    <img class="w-100 h-100 object-fit-cover rounded-circle" src="../assets/static/user.png">
+            <nav class="d-flex justify-content-between align-items-center p-4">
+                <a class="text-decoration-none" href="./home.html">
+                    <h1 class="fuente-titular">VOTAPP</h1>
                 </a>
-            </div>
-        </nav>
-    </header>
+        
+                <div class="d-flex gap-5 fw-semibold" style="color:var(--texto-variante);">
+                    <a href="elecciones.html" class="text-decoration-none text-reset">Elecciones</a>
+                    <a href="resultados.html" class="text-decoration-none text-reset">Resultados</a>
+                    <a href="candidatos.html" class="text-decoration-none text-reset">Candidatos</a>
+                </div>
+        
+                <div class="d-flex gap-3 align-items-center">
+                    <a href="info.html" class="text-decoration-none text-reset"><span class="material-symbols-outlined">info</span></a>
+                    <a href="../index.html" class="text-decoration-none text-reset"><span class="material-symbols-outlined">logout</span></a>
+                    <!-- TODO arreglar ruta logout esta cifrada por api -->
+                    <a href="" class="d-block text-decoration-none text-reset" style="width: 40px; height: 40px;">
+                        <img class="w-100 h-100 object-fit-cover rounded-circle" src="../assets/public/user.png">
+                    </a>
+                </div>
+            </nav>
+        </header>
         `
     }
 }
